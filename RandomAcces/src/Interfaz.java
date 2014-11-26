04ed90714b0c98bbda767c061e2381ae9452f24b
miguel <miguel@MIguel-PC.alumnos.uem.es>
2014-11-26 19:51:35 +0100

    mierdaaaaaaaa

--- RandomAcces/src/Interfaz.java
+++ RandomAcces/src/Interfaz.java
@@ -1,6 +1,17 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollBar;
 
 
 public class Interfaz {
@@ -37,6 +48,28 @@ public class Interfaz {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnInicio = new JMenu("Inicio");
+		menuBar.add(mnInicio);
+		
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mnInicio.add(mntmSalir);
+		
+		JMenu mnLibros = new JMenu("Libros");
+		menuBar.add(mnLibros);
+		
+		JMenuItem mntmAadirLibros = new JMenuItem("A\u00F1adir Libros");
+		mnLibros.add(mntmAadirLibros);
 	}
-
 }
